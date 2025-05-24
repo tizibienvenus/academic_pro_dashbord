7da11719c06cdd08f80ac920491e48b6c568c9c1
tizibinvenus <tizibienvenus@gmail.com>
2025-05-24 11:59:40 +0100

    V3

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "913214299"
+    serviceWorkerVersion: "3626680034"
   }
 });
